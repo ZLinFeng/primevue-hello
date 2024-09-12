60db7e840e9196522884e05b779c9b6e07f6fd76
Zhu <1@qq.com>
2024-09-12 17:56:19 +0800

    color

--- src/components/form/LoginForm.vue
+++ src/components/form/LoginForm.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Password from "primevue/password"
 import FloatLabel from "primevue/floatlabel"
+import Button from "primevue/button"
 import { ref } from "vue"
 import SvgIcon from "../icon/SvgIcon.vue"
 import InputText from "primevue/inputtext"
+import { RouterLink } from "vue-router"
 
 const props = defineProps({
     name: {
@@ -17,6 +19,19 @@ const props = defineProps({
 })
 
 const passwordValue = ref("")
+const usernameValue = ref("")
+const passwordActive = ref(false)
+const usernameActive = ref(false)
+
+const foucsPassword = () => {
+    passwordActive.value = true
+    usernameActive.value = false
+}
+
+const focusUsername = () => {
+    passwordActive.value = false
+    usernameActive.value = true
+}
 </script>
 
 <template>
@@ -24,43 +39,67 @@ const passwordValue = ref("")
         <span class="text-2xl text-center mb-4 mt-12">
             {{ props.name }} Login
         </span>
-        <span class="text-xs text-center line-clamp-2 leading-loose text-slate-800">
+        <span class="text-xs text-center line-clamp-2 leading-loose text-slate-400">
             {{ props.desc }}
         </span>
-        <div class="flex h-[40px] mt-[30px] gap-2 place-content-center">
-            <div class="place-content-center">
-                <SvgIcon
-                    name="username"
-                    :size="25"
-                    type="primary"
-                />
+        <div class="flex justify-center mt-[30px]">
+            <div class="w-[300px] flex h-[40px] gap-6 place-content-center">
+                <div class="place-content-center">
+                    <SvgIcon
+                        name="username"
+                        :size="25"
+                        :type="usernameActive ? 'primary' : 'info'"
+                    />
+                </div>
+                <FloatLabel>
+                    <InputText
+                        v-model="usernameValue"
+                        input-id="passwd"
+                        class="w-[250px]"
+                        @focus="focusUsername"
+                        @focusout="console.log('hello')"
+                    />
+                    <label for="passwd">Username</label>
+                </FloatLabel>
             </div>
-            <FloatLabel>
-                <InputText
-                    v-model="passwordValue"
-                    input-id="passwd"
-                    class="w-[250px]"
-                />
-                <label for="passwd">Username</label>
-            </FloatLabel>
         </div>
-        <div class="flex h-[40px] mt-[30px] gap-2 place-content-center">
-            <div class="place-content-center">
-                <SvgIcon
-                    name="password"
-                    :size="25"
-                    type="info"
-                />
+        <div class="flex justify-center mt-[30px]">
+            <div class="flex h-[40px] gap-6 place-content-center w-[300px]">
+                <div class="place-content-center">
+                    <SvgIcon
+                        name="password"
+                        :size="25"
+                        :type="passwordActive ? 'primary' : 'info'"
+                    />
+                </div>
+                <FloatLabel>
+                    <Password
+                        v-model="passwordValue"
+                        input-id="passwd"
+                        input-class="w-[250px]"
+                        :feedback="false"
+                        toggle-mask
+                        @focus="foucsPassword"
+                    />
+                    <label
+                        for="passwd"
+                    >Password</label>
+                </FloatLabel>
             </div>
-            <FloatLabel>
-                <Password
-                    v-model="passwordValue"
-                    toggle-mask
-                    input-id="passwd"
-                    input-class="w-[250px]"
-                />
-                <label for="passwd">Password</label>
-            </FloatLabel>
+        </div>
+        <div class="flex justify-center mt-[30px]">
+            <Button class="w-[300px]">
+                Sign in
+            </Button>
+        </div>
+        <div class="flex justify-center mt-[30px] place-items-center gap-2">
+            <span class="text-xs text-slate-400">Don't have an account?</span>
+            <RouterLink
+                to="/auth/register"
+                class="text-xs"
+            >
+                Request Now
+            </RouterLink>
         </div>
     </div>
 </template>
