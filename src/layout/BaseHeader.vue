23238d7e3ca4fceb7552c67b7f791e0ffdbffd25
Zhu <1@qq.com>
2024-11-15 21:17:12 +0800

    nemu

--- src/layout/BaseHeader.vue
+++ src/layout/BaseHeader.vue
@@ -1,75 +1,62 @@
 <script setup lang="ts">
-import IconField from "primevue/iconfield"
-import InputIcon from "primevue/inputicon"
-import InputText from "primevue/inputtext"
-import Avatar from "primevue/avatar"
-import {ref} from "vue"
-import SvgIcon from "@/components/icon/SvgIcon.vue"
-import avatarUtl from "@/assets/images/avatar.png"
+import Menubar from "primevue/menubar"
+import { ref } from "vue"
 
-const searchInput = ref("")
+const items = ref([
+    {
+        label: "Home",
+        icon: "pi pi-home"
+    },
+    {
+        label: "Features",
+        icon: "pi pi-star"
+    },
+    {
+        label: "Projects",
+        icon: "pi pi-search",
+        items: [
+            {
+                label: "Components",
+                icon: "pi pi-bolt"
+            },
+            {
+                label: "Blocks",
+                icon: "pi pi-server"
+            },
+            {
+                label: "UI Kit",
+                icon: "pi pi-pencil"
+            },
+            {
+                label: "Templates",
+                icon: "pi pi-palette",
+                items: [
+                    {
+                        label: "Apollo",
+                        icon: "pi pi-palette"
+                    },
+                    {
+                        label: "Ultima",
+                        icon: "pi pi-palette"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: "Contact",
+        icon: "pi pi-envelope"
+    }
+])
 
 </script>
 
 <template>
-    <div class="flex items-center gap-2 pr-8">
-        <div class="flex-grow" />
-        <div>
-            <IconField class="flex items-center">
-                <InputIcon>
-                    <span class="pi pi-search text-black" />
-                </InputIcon>
-                <InputText
-                    v-model="searchInput"
-                    class="h-[30px] w-[180px] search-input"
-                    placeholder="Search"
-                />
-                <InputIcon
-                    class=""
-                >
-                    <div class="border-[1px] border-slate-400 bg-slate-100 rounded">
-                        <span class="text-xs m-1 text-black">⌘+S</span>
-                    </div>
-                </InputIcon>
-            </IconField>
-        </div>
-        <button
-            class="p-[4px] content-center border-[1px] rounded
-        cursor-pointer border-slate-400 bg-slate-100 text-black hover:border-black focus:text-white focus:bg-black focus:border-black"
-        >
-            <SvgIcon
-                :size="18"
-                name="alert"
-            />
-        </button>
-        <button
-            class="p-[4px] content-center border-[1px] rounded
-        cursor-pointer border-slate-400 bg-slate-100 text-black hover:border-black focus:text-white focus:bg-black focus:border-black"
-        >
-            <SvgIcon
-                :size="18"
-                name="settings"
-            />
-        </button>
-        <button class="flex items-center">
-            <Avatar
-                :image="avatarUtl"
-                shape="circle"
-            />
-        </button>
+    <div class="h-12">
+        <Menubar :model="items" />
     </div>
 </template>
 
 <style scoped>
-.search-input {
-  border: 1px solid #94a3b8 !important;
-}
 
-.search-input:hover {
-  border-color: black !important;
-}
-
-.search-input:focus {
-  border-color: black !important;
-}
 </style>
--- src/layout/BaseLayout.vue
+++ src/layout/BaseLayout.vue
@@ -1,72 +1,12 @@
 <script setup lang="ts">
-import BaseSider from "./BaseSider.vue"
-import Divider from "primevue/divider"
-import BaseHeader from "@/layout/BaseHeader.vue"
-import {RouterView} from "vue-router"
-import ScrollPanel from "primevue/scrollpanel"
-import {computed, ref, onMounted, onUnmounted} from "vue"
-
-const screenWidth = ref(window.innerWidth)
-
-const updateWidth = () => {
-    screenWidth.value = window.innerWidth
-    console.log(screenWidth.value)
-}
-
-onMounted(() => {
-    window.addEventListener("resize", updateWidth)
-})
-
-onUnmounted(() => {
-    window.removeEventListener("resize", updateWidth)
-})
-
-const computedWidth = computed(() => {
-    console.log(`computed: ${screenWidth.value - 241}px`)
-    return `${screenWidth.value - 241}px`
-})
-
-const computedContainWidth = computed(() => {
-    console.log(`computed: ${screenWidth.value - 241 - 20}px`)
-    return `${screenWidth.value - 241 - 20}px`
-})
-
-
+import BaseHeader from "./BaseHeader.vue"
 </script>
 
 <template>
-    <div class="flex h-screen w-screen">
-        <div class="w-[240px] h-full">
-            <BaseSider class="w-full h-full" />
-        </div>
-        <Divider
-            layout="vertical"
-            style="margin: 0; padding: 0;"
-            class="w-[1px]"
-        />
-        <div
-            class="flex flex-col grow bg-slate-200"
-            :style="{width: computedWidth}"
-        >
-            <div class="bg-white h-[50px] flex items-center justify-end w-full">
-                <BaseHeader />
-            </div>
-            <div
-                class="flex-grow mt-10 mb-2 mx-[10px] bg-white rounded-2xl flex flex-col"
-                :style="{width: computedContainWidth}"
-            >
-                <scroll-panel
-                    class="flex-grow h-[500px] w-full"
-                    :dt="{
-                        bar: {
-                            background: 'black'
-                        }
-                    }"
-                >
-                    <RouterView />
-                </scroll-panel>
-            </div>
-        </div>
+    <div
+        class="flex flex-col"
+    >
+        <BaseHeader />
     </div>
 </template>
 
